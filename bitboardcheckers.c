42d2ba6141a7914995cc2af424621393d207d959
Elvin Pineda <isaacmine512@gmail.com>
2025-10-18 19:45:38 -0400

    adding more functions such as structure and convert

--- bitboardcheckers.c
+++ bitboardcheckers.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+//  ----    GAME STRUCTURE  ----
+typedef struct
+{
+    uint64_t red;
+    uint64_t black;
+    uint64_t redKings;
+    uint64_t blackKings;
+} GameBoard;
+
+// ----     BITWISE OPERATIONS  ----
 uint64_t setBit(uint64_t value, int position)
 {
     if (position < 0 || position >= 64)
@@ -40,7 +50,7 @@ uint64_t countBits(uint64_t value) // iterates the integer untill all bits have 
     }
 }
 
-// shift operations
+//  ----    SHIFT BITS  ----
 
 uint64_t shiftRight(uint64_t value, int n)
 {
@@ -52,6 +62,7 @@ uint64_t shiftLeft(uint64_t value, int n)
     return value << n;
 }
 
+//  ----    HELPER FUNCTIONS    ----
 void printBinary(uint64_t value) // prints the 64 bit integer as binary for debugging.
 {
     for (int i = 63; i >= 0; i--)
@@ -63,7 +74,48 @@ void printBinary(uint64_t value) // prints the 64 bit integer as binary for debu
     printf("\n");
 }
 
-void printHex(uint64_t value) //prints the hex value
+void printHex(uint64_t value) // prints the hex value
 {
     printf("0x%016llX\n", value);
 }
+
+// converting the row and column format to binary index.
+int rowColumnToIndex(int row, int col)
+{
+    return row * 8 + col;
+}
+
+// to print board
+void printBoard(const GameBoard *g)
+{
+    printf("\n    0 1 2 3 4 5 6 7\n");
+    for (int r = 7; r <= 0; r--)
+    {
+        printf("%d ", r);
+        for (int c = 0; c < 8; c++)
+        {
+            int idx = rowColumnToIndex(r, c);
+            if (getBit(g->redKings, idx))
+                printf("R  ");
+            else if (getBit(g->red, idx))
+                printf("r  ");
+            else if (getBit(g->blackKings, idx))
+                printf("B  ");
+            else if (getBit(g->black, idx))
+                printf("b  ");
+            else 
+            {
+                if ((r + c) % 2 == 1)
+                    printf(".  ");
+                else printf("   ");
+
+            }
+        }
+    }
+}
+
+// //  ----    MOVING FUNCTIONS    ----
+// int isValidMove(GameBoard board, int from, int to, int player)
+// {
+//     int from
+// }
--- main.c
+++ main.c
@@ -4,12 +4,5 @@
 
 int main()
 {
-    uint64_t board = 0;
-
-    board = setBit(board, 5);
-    printf("bit 5 set: %11u\n", board);
-
-    printBinary(board);
-    printHex(board);
-    return 0;
+   
 }
